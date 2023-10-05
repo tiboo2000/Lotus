5c8815f53877fd7e9165de754550465e9ad5d3c8
tiboo2000 <thibaud.danneau@std.heh.be>
2023-10-05 09:16:17 +0200

    first

---
+++ Lotus/src/test/java/be/heh/lotus/LotusApplicationTests.java
@@ -0,0 +1,13 @@
+package be.heh.lotus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
