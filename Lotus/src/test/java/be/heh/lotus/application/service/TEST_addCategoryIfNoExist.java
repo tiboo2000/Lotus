b76318d45c1b23b969d732506f62be24ebbf9632
KouptchN <kouptchinskyng@gmail.com>
2023-10-09 15:12:47 +0200

    light name update

--- Lotus/src/test/java/be/heh/lotus/application/service/TEST_addCategoryIfNoExist.java
+++ Lotus/src/test/java/be/heh/lotus/application/service/TEST_addCategoryIfNoExist.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 public class TEST_addCategoryIfNoExist {
-    private static final Categories category=new Categories(1,"Ordinateur");
-    private static final boolean condition = false;
+    private static final Categories CATEGORY=new Categories(1,"Ordinateur");
+    private static final boolean CONDITION = false;
     @Test
     public void shouldAddCategory(){
         Categories_Out categoriesOut = mock(Categories_Out.class);
         GestionCategories gestionCategories =new GestionCategories(categoriesOut);
-        when(categoriesOut.verifyCategories(category.getId())).thenReturn(condition);
-        gestionCategories.add(category);
-        verify(categoriesOut).addCategory(category);
+        when(categoriesOut.verifyCategories(CATEGORY.getId())).thenReturn(CONDITION);
+        gestionCategories.add(CATEGORY);
+        verify(categoriesOut).addCategory(CATEGORY);
     }
 }
