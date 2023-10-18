a549fe765ed8901813576e88e99a7f44db78cb47
tiboo2000 <thibaud.danneau@std.heh.be>
2023-10-18 14:18:17 +0200

    Update UseCase_In_Categories.java

--- Lotus/src/main/java/be/heh/lotus/port/in/UseCase_In_Categories.java
+++ Lotus/src/main/java/be/heh/lotus/port/in/UseCase_In_Categories.java
@@ -3,8 +3,5 @@ package be.heh.lotus.port.in;
 import be.heh.lotus.application.domain.Categories;
 
 public interface UseCase_In_Categories {
-
-    void add(Categories category);
-    public void remove(Categories category);
-    public Categories get();
+    
 }
