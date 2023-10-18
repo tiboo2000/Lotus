c51d262635a1d80447bc7be366e4556427995f2d
KouptchN <kouptchinskyng@gmail.com>
2023-10-18 13:55:30 +0200

    update

--- lotusreact/src/App.js
+++ lotusreact/src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const axios = require('axios');
+
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://virtserver.swaggerhub.com/KOUPTCHINSKYNG/test/1.0.1/category/0',
+    headers: { }
+  };
+
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
