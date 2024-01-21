21fa0dc728227b0d2e2fc5a87193588b11d3dffe
Maximilien <maxzbil1@gmail.com>
2024-01-21 22:09:43 +0100

    fini : config modif + request

--- Lotus/src/main/java/be/heh/lotus/Config.java
+++ Lotus/src/main/java/be/heh/lotus/Config.java
@@ -1,9 +1,14 @@
 package be.heh.lotus;
 
+import be.heh.lotus.adapter.out.persistance.BagAdapterPersistence;
 import be.heh.lotus.adapter.out.persistance.CategoryAdapterPersistence;
+import be.heh.lotus.adapter.out.persistance.repository.BagRepository;
 import be.heh.lotus.adapter.out.persistance.repository.CategoriesRepository;
 import be.heh.lotus.application.domain.service.GestionCategories;
+import be.heh.lotus.application.domain.service.Gestion_Bag;
+import be.heh.lotus.application.port.in.UseCase_In_Bag;
 import be.heh.lotus.application.port.in.UseCase_In_Categories;
+import be.heh.lotus.application.port.out.Bag_Out;
 import be.heh.lotus.application.port.out.Categories_Out;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,8 +21,17 @@ public class Config {
     @Autowired
     CategoriesRepository categoriesRepository;
     Categories_Out categoriesOut;
+
+    @Autowired
+    BagRepository bagRepository;
+    Bag_Out bagOut;
     @Bean
     public UseCase_In_Categories getCategoryUseCase(){
         return new GestionCategories(new CategoryAdapterPersistence(categoriesRepository));
+    }
+
+    @Bean
+    public UseCase_In_Bag getBagUseCase(){
+        return new Gestion_Bag(new BagAdapterPersistence(bagRepository));
     }
 }
--- Lotus/src/main/java/be/heh/lotus/adapter/in/web/AdapterWeb.java
+++ Lotus/src/main/java/be/heh/lotus/adapter/in/web/AdapterWeb.java
@@ -1,6 +1,9 @@
 package be.heh.lotus.adapter.in.web;
 
+import be.heh.lotus.application.domain.model.Bag;
 import be.heh.lotus.application.domain.model.Categories;
+import be.heh.lotus.application.domain.model.Product;
+import be.heh.lotus.application.port.in.UseCase_In_Bag;
 import be.heh.lotus.application.port.in.UseCase_In_Categories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +16,12 @@ import java.util.List;
 @RestController
 public class AdapterWeb {
     private UseCase_In_Categories categoriesUseCase;
-    public AdapterWeb(UseCase_In_Categories categoriesUseCase) {this.categoriesUseCase=categoriesUseCase;}
+
+    private UseCase_In_Bag bagUseCase; //TO ADD
+    public AdapterWeb(UseCase_In_Categories categoriesUseCase, UseCase_In_Bag bagUseCase) {
+        this.bagUseCase=bagUseCase;//TO ADD
+        this.categoriesUseCase=categoriesUseCase;
+    }
     @GetMapping("/categories")
     public ResponseEntity<List<Categories>> getAllCategoriesWeb(){
         List<Categories> categoriesArrayList=categoriesUseCase.get();
@@ -46,5 +54,57 @@ public class AdapterWeb {
         }
         categoriesUseCase.removeById(id);
         return ResponseEntity.status(HttpStatus.CREATED).body("Category deleted successfully");
+    }
+    //Max Bag
+    @GetMapping("/bag")
+    public ResponseEntity<Bag> getUserBag(@RequestParam(required = true)String user){
+        Bag bagArrayList=bagUseCase.getBagUser(user);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(bagArrayList);
+    }
+    @GetMapping("/bag/getquantity")
+    public ResponseEntity<Integer> getBagWeb(@RequestParam(required = true) Product product, @RequestParam(required = true) String nameuser, Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        int bag=bagUseCase.getQuantity(product,nameuser);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(bag);
+    }
+    @PostMapping("/bag/add")
+    public ResponseEntity<String> addBagWeb(@Validated @RequestParam(required = true)String nameuser, @Validated @RequestBody Product product, Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Bag added");
+        }
+        bagUseCase.AddToBag(product, nameuser);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Bag added successfully");
+    }
+    @DeleteMapping("/bag/delete")
+    public ResponseEntity<String> removeBagWeb(@RequestParam(required = true)String nameuser,Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Bag deleted");
+        }
+        bagUseCase.ResetBag(nameuser);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Bag deleted successfully");
+    }
+
+    @DeleteMapping("/bag/delete/product")
+    public ResponseEntity<String> removeBagWeb(@RequestParam(required = true)String nameuser, @RequestParam(required = true)Product product,Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Product deleted");
+        }
+        bagUseCase.SuppFromBag(product, nameuser);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Product deleted successfully");
+    }
+
+    @PutMapping("/bag/update")
+    public ResponseEntity<String> updateBagWeb(@RequestParam(required = true)String nameuser, @RequestParam(required = true)Product product, @RequestParam(required = true)int quantity, @RequestParam(required = true)String operation,Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Product updated");
+        }
+        bagUseCase.modifyQuantity(bagUseCase.getQuantity(product, nameuser),quantity, operation, nameuser ,product);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Quantity Product updated successfully");
     }
 }
