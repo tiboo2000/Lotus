8329660c8e0c508a3c385ac1b1b4a48726c8646d
Maximilien <maximilien.zak@std.heh.be>
2024-01-22 15:30:52 +0100

    update product controller test

--- Lotus/src/test/java/be/heh/lotus/adapter/in/web/ProductControllerTest.java
+++ Lotus/src/test/java/be/heh/lotus/adapter/in/web/ProductControllerTest.java
@@ -1,6 +1,8 @@
 package be.heh.lotus.adapter.in.web;
 
+import be.heh.lotus.application.domain.model.Bag;
 import be.heh.lotus.application.domain.model.Product;
+import be.heh.lotus.application.port.in.UseCase_In_Bag;
 import be.heh.lotus.application.port.in.UseCase_In_Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,10 +15,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
@@ -31,6 +35,8 @@ public class ProductControllerTest {
     @Mock
     private UseCase_In_Product useCaseInProduct;
 
+    @Mock
+    private UseCase_In_Bag bagUseCase;
     @InjectMocks
     private AdapterWeb productController;
 
@@ -83,4 +89,75 @@ public class ProductControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].name").value("Product 1"));
     }
+
+    //Max test
+
+    @Test
+    public void getUserBag_ShouldReturnUserBag() throws Exception {
+        ArrayList<Product> products = new ArrayList<>();
+        Bag bag = new Bag(products, "UserTest");
+        when(bagUseCase.getBagUser("UserTest")).thenReturn(bag);
+        Product product = new Product(1, "Test Product", 100.0, 1);
+
+        mockMvc.perform(get("/bag").param("user", "UserTest"))
+                .andExpect(status().isAccepted())
+                .andExpect(jsonPath("$.product").value(product));
+    }
+
+    @Test
+    public void addBagWeb_ShouldAddToBag() throws Exception {
+        Product product = new Product(/* paramètres du produit */);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String productJson = objectMapper.writeValueAsString(product);
+
+        mockMvc.perform(post("/bag/add")
+                        .param("nameuser", "UserTest")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productJson))
+                .andExpect(status().isCreated());
+
+        verify(bagUseCase, times(1)).AddToBag(any(Product.class), eq("UserTest"));
+    }
+
+    @Test
+    public void removeBagWeb_ShouldDeleteBag() throws Exception {
+        mockMvc.perform(delete("/bag/delete")
+                        .param("nameuser", "UserTest"))
+                .andExpect(status().isCreated());
+
+        verify(bagUseCase, times(1)).ResetBag("UserTest");
+    }
+
+    @Test
+    public void removeProductFromBag_ShouldDeleteProductFromBag() throws Exception {
+        Product product = new Product(/* paramètres du produit */);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String productJson = objectMapper.writeValueAsString(product);
+
+        mockMvc.perform(delete("/bag/delete/product")
+                        .param("nameuser", "UserTest")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productJson))
+                .andExpect(status().isCreated());
+
+        verify(bagUseCase, times(1)).SuppFromBag(any(Product.class), eq("UserTest"));
+    }
+
+    @Test
+    public void updateBagWeb_ShouldUpdateBag() throws Exception {
+        Product product = new Product(/* paramètres du produit */);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String productJson = objectMapper.writeValueAsString(product);
+
+        mockMvc.perform(put("/bag/update")
+                        .param("nameuser", "UserTest")
+                        .param("quantity", "3")
+                        .param("operation", "add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productJson))
+                .andExpect(status().isCreated());
+
+        verify(bagUseCase, times(1)).modifyQuantity(1, eq(3), eq("add"), eq("UserTest"), any(Product.class));
+    }
+
 }
