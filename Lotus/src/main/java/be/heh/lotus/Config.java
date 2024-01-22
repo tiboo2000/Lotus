467d83565d88d343cf7e77bc1df5b0ee1763dda0
Maximilien <maximilien.zak@std.heh.be>
2024-01-22 14:35:30 +0100

    change config and adapter for merge

--- Lotus/src/main/java/be/heh/lotus/Config.java
+++ Lotus/src/main/java/be/heh/lotus/Config.java
@@ -1,18 +1,18 @@
 package be.heh.lotus;
 
-import be.heh.lotus.adapter.out.persistance.BagAdapterPersistence;
 import be.heh.lotus.adapter.out.persistance.CategoryAdapterPersistence;
-import be.heh.lotus.adapter.out.persistance.repository.BagRepository;
+import be.heh.lotus.adapter.out.persistance.ProductAdapterPersistence;
 import be.heh.lotus.adapter.out.persistance.repository.CategoriesRepository;
+import be.heh.lotus.adapter.out.persistance.repository.ProductRepository;
 import be.heh.lotus.application.domain.service.GestionCategories;
-import be.heh.lotus.application.domain.service.Gestion_Bag;
-import be.heh.lotus.application.port.in.UseCase_In_Bag;
+import be.heh.lotus.application.domain.service.Gestion_Product;
 import be.heh.lotus.application.port.in.UseCase_In_Categories;
-import be.heh.lotus.application.port.out.Bag_Out;
+import be.heh.lotus.application.port.in.UseCase_In_Product;
 import be.heh.lotus.application.port.out.Categories_Out;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @Configuration
@@ -20,18 +20,15 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 public class Config {
     @Autowired
     CategoriesRepository categoriesRepository;
-    Categories_Out categoriesOut;
-
     @Autowired
-    BagRepository bagRepository;
-    Bag_Out bagOut;
+    ProductRepository productRepository;
+    Categories_Out categoriesOut;
     @Bean
     public UseCase_In_Categories getCategoryUseCase(){
         return new GestionCategories(new CategoryAdapterPersistence(categoriesRepository));
     }
-
     @Bean
-    public UseCase_In_Bag getBagUseCase(){
-        return new Gestion_Bag(new BagAdapterPersistence(bagRepository));
+    public UseCase_In_Product getProductUseCase(){
+        return new Gestion_Product(new ProductAdapterPersistence(productRepository));
     }
 }
--- Lotus/src/main/java/be/heh/lotus/adapter/in/web/AdapterWeb.java
+++ Lotus/src/main/java/be/heh/lotus/adapter/in/web/AdapterWeb.java
@@ -1,13 +1,11 @@
 package be.heh.lotus.adapter.in.web;
 
-import be.heh.lotus.application.domain.model.Bag;
 import be.heh.lotus.application.domain.model.Categories;
 import be.heh.lotus.application.domain.model.Product;
-import be.heh.lotus.application.port.in.UseCase_In_Bag;
 import be.heh.lotus.application.port.in.UseCase_In_Categories;
+import be.heh.lotus.application.port.in.UseCase_In_Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,96 +13,67 @@ import java.util.List;
 
 @RestController
 public class AdapterWeb {
-    private UseCase_In_Categories categoriesUseCase;
+    private final UseCase_In_Categories categoriesUseCase;
+    private final UseCase_In_Product productUseCase;
 
-    private UseCase_In_Bag bagUseCase; //TO ADD
-    public AdapterWeb(UseCase_In_Categories categoriesUseCase, UseCase_In_Bag bagUseCase) {
-        this.bagUseCase=bagUseCase;//TO ADD
-        this.categoriesUseCase=categoriesUseCase;
+    public AdapterWeb(UseCase_In_Categories categoriesUseCase, UseCase_In_Product productUseCase) {
+        this.categoriesUseCase = categoriesUseCase;
+        this.productUseCase = productUseCase;
     }
+
+    // Categories
     @GetMapping("/categories")
-    public ResponseEntity<List<Categories>> getAllCategoriesWeb(){
-        List<Categories> categoriesArrayList=categoriesUseCase.get();
-        return ResponseEntity
-                .status(HttpStatus.ACCEPTED)
-                .body(categoriesArrayList);
+    public ResponseEntity<List<Categories>> getAllCategories() {
+        List<Categories> categories = categoriesUseCase.get();
+        return ResponseEntity.ok(categories);
     }
-    @GetMapping("/category")
-    public ResponseEntity<Categories> getCategoryWeb(@RequestParam(required = true)int id, Errors errors){
-        if(errors.hasErrors()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        List<Categories> category=categoriesUseCase.getById(id);
-        return ResponseEntity
-                .status(HttpStatus.ACCEPTED)
-                .body(category.get(0));
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<Categories> getCategoryById(@PathVariable int id) {
+        Categories category = categoriesUseCase.getById(id).stream().findFirst().orElse(null);
+        return category != null ?
+                ResponseEntity.ok(category) :
+                ResponseEntity.notFound().build();
     }
+
     @PostMapping("/category")
-    public ResponseEntity<String> addCategoryWeb(@Validated @RequestBody Categories category, Errors errors){
-        if(errors.hasErrors()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Category added");
-        }
+    public ResponseEntity<String> addCategory(@Validated @RequestBody Categories category) {
         categoriesUseCase.add(category);
         return ResponseEntity.status(HttpStatus.CREATED).body("Category added successfully");
     }
-    @DeleteMapping("/category")
-    public ResponseEntity<String> removeCategoryWeb(@RequestParam(required = true)int id,Errors errors){
-        if(errors.hasErrors()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Category deleted");
-        }
+
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<String> removeCategory(@PathVariable int id) {
         categoriesUseCase.removeById(id);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Category deleted successfully");
-    }
-    //Max Bag
-    @GetMapping("/bag")
-    public ResponseEntity<Bag> getUserBag(@RequestParam(required = true)String user){
-        Bag bagArrayList=bagUseCase.getBagUser(user);
-        return ResponseEntity
-                .status(HttpStatus.ACCEPTED)
-                .body(bagArrayList);
-    }
-    @GetMapping("/bag/getquantity")
-    public ResponseEntity<Integer> getBagWeb(@RequestBody(required = true) Product product, @RequestParam(required = true) String nameuser, Errors errors){
-        if(errors.hasErrors()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        int bag=bagUseCase.getQuantity(product,nameuser);
-        return ResponseEntity
-                .status(HttpStatus.ACCEPTED)
-                .body(bag);
-    }
-    @PostMapping("/bag/add")
-    public ResponseEntity<String> addBagWeb(@Validated @RequestParam(required = true)String nameuser, @Validated @RequestBody Product product, Errors errors){
-        if(errors.hasErrors()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Bag added");
-        }
-        bagUseCase.AddToBag(product, nameuser);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Bag added successfully");
-    }
-    @DeleteMapping("/bag/delete")
-    public ResponseEntity<String> removeBagWeb(@RequestParam(required = true)String nameuser,Errors errors){
-        if(errors.hasErrors()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Bag deleted");
-        }
-        bagUseCase.ResetBag(nameuser);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Bag deleted successfully");
+        return ResponseEntity.ok("Category deleted successfully");
     }
 
-    @DeleteMapping("/bag/delete/product")
-    public ResponseEntity<String> removeBagWeb(@RequestParam(required = true)String nameuser, @RequestBody(required = true)Product product,Errors errors){
-        if(errors.hasErrors()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Product deleted");
-        }
-        bagUseCase.SuppFromBag(product, nameuser);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Product deleted successfully");
+    // Products
+    @PostMapping("/product")
+    public ResponseEntity<String> addProduct(@Validated @RequestBody Product product) {
+        productUseCase.addProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Product added successfully");
     }
 
-    @PutMapping("/bag/update")
-    public ResponseEntity<String> updateBagWeb(@RequestParam(required = true)String nameuser, @RequestBody(required = true)Product product, @RequestParam(required = true)int quantity, @RequestParam(required = true)String operation,Errors errors){
-        if(errors.hasErrors()){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed Product updated");
-        }
-        bagUseCase.modifyQuantity(bagUseCase.getQuantity(product, nameuser),quantity, operation, nameuser ,product);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Quantity Product updated successfully");
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable int productId) {
+        Product product = productUseCase.getProductById(productId);
+        return product != null ?
+                ResponseEntity.ok(product) :
+                ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/product/{productId}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int productId) {
+        productUseCase.deleteProduct(productId);
+        return ResponseEntity.ok("Product deleted successfully");
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProductsByCategory(@RequestParam int idCategory) {
+        List<Product> products = productUseCase.getAllProductsByCategoryId(idCategory);
+        return products != null && !products.isEmpty() ?
+                ResponseEntity.ok(products) :
+                ResponseEntity.notFound().build();
     }
 }
