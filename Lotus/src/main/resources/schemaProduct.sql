320ced83b36f7b4aec0e4c34233af1314952c69f
Maximilien <maximilien.zak@std.heh.be>
2024-01-22 15:17:41 +0100

    change schemaproduct sql

--- Lotus/src/main/resources/schemaProduct.sql
+++ Lotus/src/main/resources/schemaProduct.sql
@@ -1,1 +1,6 @@
-ALTER TABLE product RENAME TO products
+CREATE TABLE products (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL,
+                          price DECIMAL(10, 2) NOT NULL,
+                          category_id INTEGER
+);
